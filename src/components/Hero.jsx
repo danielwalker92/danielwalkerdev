88ff64a1b90a815801eb6755eaf4660966b87d8b
Daniel Walker <danielrywalker@gmail.com>
2025-10-05 10:54:07 -0700

    Updating Portfolio, Nothing Special

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -155,7 +155,7 @@ export default function Hero() {
               <span className="block text-blue-500 xl:inline">Software Engineer</span>
             </h1>
             <p className="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
-              I'm so glad you're here, I can't wait to show you what I've been working on.
+              I'm glad you're here, I can't wait to show you what I've been working on.
             </p>
             <div className="mx-auto mt-5 max-w-md sm:flex sm:justify-center md:mt-8">
               <div className="rounded-md shadow">
